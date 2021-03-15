03b614795777544139fb8de05667bb8f55065dd0
biben-hub <bilombob@gmail.com>
2021-03-15 18:28:46 +0100

    test git action

--- test_reverse.py
+++ test_reverse.py
@@ -1,9 +1,12 @@
 import unittest
+import os
+
+from reverse import reverse
 
 class Testdefreverse(unittest.TestCase):
     
     def test_reverse_is_reversing(self):
-        self.assertEqual(reverse)
+        self.assertEqual(reverse("test"))
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
